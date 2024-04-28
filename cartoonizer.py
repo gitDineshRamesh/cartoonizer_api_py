69e63b8cca1d39184845f73666bff9cf1cc0449c
gitDineshRamesh <156403211+gitDineshRamesh@users.noreply.github.com>
2024-04-28 02:06:24 +0000

    modified:   cartoonizer.py 	modified:   requirements.txt Changed to tensorflow lite version

--- cartoonizer.py
+++ cartoonizer.py
@@ -3,7 +3,7 @@ from PIL import Image
 from io import BytesIO
 import cv2
 import base64
-import tensorflow as tf
+import tensorflow.lite as lite
 
 def loadImage(base64string):
     if "base64," in base64string:
@@ -15,13 +15,21 @@ def loadImage(base64string):
     Cvimg = cv2.cvtColor(np_arr, cv2.COLOR_RGB2BGR)
     return Cvimg
 
-def preprocessImage(img,td=224):
-    shp = tf.cast(tf.shape(img)[1:-1], tf.float32)
-    sd  = min(shp)
-    scl = td/sd
-    nhp = tf.cast(shp*scl, tf.int32)
-    img = tf.image.resize(img,nhp)
-    img = tf.image.resize_with_crop_or_pad(img, td,td)
+def preprocessImage(img, td=224):
+    shp = img.shape[:2]  # Get height and width
+    scale = min(shp) / td  # Calculate scaling factor
+    new_size = tuple(int(dim * scale) for dim in shp)  # Calculate new dimensions
+
+    # Resize and center crop the image
+    img = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
+    pad_h = td - img.shape[0]
+    pad_w = td - img.shape[1]
+    top, bottom = pad_h // 2, pad_h // 2 + pad_h % 2
+    left, right = pad_w // 2, pad_w // 2 + pad_w % 2
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+
+    # Convert to float32 and normalize to range [0, 1]
+    img = img.astype(np.float32) / 255.0
     return img
 
 def postprocessImage(img):
@@ -38,16 +46,23 @@ def cartoon(img_p):
 
     psi = preprocessImage(si,td=512)
     psi.shape
+    #psi = psi.astype(np.float32)
+    # Load the TensorFlow Lite model
+    interpreter = lite.Interpreter(model_path="1.tflite")
+    interpreter.allocate_tensors()
 
-    # model dataflow 
-    m = '1.tflite'
-    i = tf.lite.Interpreter(model_path=m)
-    ind = i.get_input_details()
-    i.allocate_tensors()
-    i.set_tensor(ind[0]['index'],psi)
-    i.invoke()
+    # Get input and output details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
 
-    r = i.tensor(i.get_output_details()[0]['index'])()
+    # Set input tensor
+    interpreter.set_tensor(input_details[0]["index"], np.expand_dims(psi, axis=0))
+
+    # Run inference
+    interpreter.invoke()
+
+    # Get output tensor
+    r = interpreter.get_tensor(output_details[0]["index"])[0]
 
     # post process the model output
     base64str = postprocessImage(r)
